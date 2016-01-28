325d3ad106cb8651db988f0a552f3255d946fc0d
mishe <mishe@163.com>
2016-01-28 16:49:10 +0800

    增加off函数，修正on事件绑定不记录绑定事件handle的bug

--- on.js
+++ on.js
@@ -1,26 +1,42 @@
 (function () {
-        
-
         function $(selector){
             return document.querySelectorAll(selector)||[];
         }
 
+        function uniqID(string){
+            string = string || '';
+            return string + Math.random().toString(36).substr(2, 10);
+        }
+
+        function evalDateset(old,nd){
+            if(old){
+                return old+'|'+nd;
+            }else{
+                return nd;
+            }
+        }
+
 
         NodeList.prototype.on = function (event,selector,fn) {
-            var self=Array.prototype.slice.call(this);
+            var self=Array.prototype.slice.call(this),eid=uniqID();
+            window[eid]=fn;
+            console.log(eid);
+
             if(typeof(selector)==='function'){
                 fn=selector;
                 selector='';
             }
             if(!selector){
-                return self.forEach(function() {
-                    this.addEventListener(event, fn);
+                return self.forEach(function(el) {
+                    el.dataset[event]=evalDateset(el.dataset[event],eid);
+                    console.log(el.dataset[event]);
+                    el.addEventListener(event, window[eid]);
                 });
             }
-            return self.forEach(function() {
+            return self.forEach(function(el) {
                 var oldFn=fn;
                 if(selector){
-                   fn=function(e){
+                    window[eid]=function(e){
                        var els=$(selector),
                            len=els.length,
                            obj= e.target,
@@ -42,9 +58,32 @@
                        }
                    }
                 }
-                this.addEventListener(event, fn);
+                el.dataset[event]=evalDateset(el.dataset[event],eid);
+                console.log(el.dataset[event]);
+                el.addEventListener(event, window[eid]);
             });
             return this;
         }
+        NodeList.prototype.off=function(event,handle){
+            var self=Array.prototype.slice.call(this);
+            self.forEach(function(el){
+                if(!event){
+                    for(var k in el.dataset){
+                        handle = el.dataset[k].split('|');
+                        for (var i = 0; i < handle.length; i++)
+                            el.removeEventListener(k, window[handle[i]]);
+                    }
+                }else {
+                    if(handle){
+                        el.removeEventListener(event, handle);
+                    }else {
+                        handle = el.dataset[event].split('|');
+                        for (var i = 0; i < handle.length; i++)
+                            el.removeEventListener(event, window[handle[i]]);
+                    }
+                }
+
+            })
+        }
         window.$=$;
-    }());
+}());
